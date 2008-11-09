e7a7d04ac9322f3510f835619447491e77d7c16a
nuuneoi <sittiphol@gmail.com>
2008-11-10 01:55:58 +0700

    Create QWERTY KCM file for Thai Keyboard and also modified Android.mk to build that file.

--- emulator/keymaps/Android.mk
+++ emulator/keymaps/Android.mk
@@ -7,6 +7,10 @@ include $(CLEAR_VARS)
 LOCAL_SRC_FILES := qwerty2.kcm
 include $(BUILD_KEY_CHAR_MAP)
 
+include $(CLEAR_VARS)
+LOCAL_SRC_FILES := qwerty_thai.kcm
+include $(BUILD_KEY_CHAR_MAP)
+
 file := $(TARGET_OUT_KEYLAYOUT)/qwerty.kl
 ALL_PREBUILT += $(file)
 $(file): $(LOCAL_PATH)/qwerty.kl | $(ACP)
