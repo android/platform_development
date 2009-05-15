8ae31153aae1ec5ae016a3187a43b44697b1fd28
Sangjin Yun <sj.yun@worksmartlabs.com>
2009-05-15 09:21:34 -0400

    Added a feature to the ApkBuilder for setting a custom KeyStore path.

--- tools/apkbuilder/src/com/android/apkbuilder/ApkBuilder.java
+++ tools/apkbuilder/src/com/android/apkbuilder/ApkBuilder.java
@@ -66,6 +66,7 @@ public final class ApkBuilder {
     private boolean mSignedPackage = true;
     /** the optional type of the debug keystore. If <code>null</code>, the default */
     private String mStoreType = null;
+    private String mKeyStore = null;
 
     /**
      * @param args
@@ -149,6 +150,11 @@ public final class ApkBuilder {
                     if (index == args.length) printUsageAndQuit();
                     
                     mStoreType  = args[index++];
+                } else if ("-keystore".equals(argument)) {
+                    // quick check on the next argument.
+                    if (index == args.length) printUsageAndQuit();
+
+                    mKeyStore  = args[index++];
                 } else {
                     printAndExit("Unknown argument: " + argument);
                 }
@@ -339,12 +345,15 @@ public final class ApkBuilder {
             SignedJarBuilder builder;
 
             if (mSignedPackage) {
+                if (mKeyStore == null) {
+                        mKeyStore = DebugKeyProvider.getDefaultKeyStoreOsPath();
+                }
+
                 System.err.println(String.format("Using keystore: %s",
-                        DebugKeyProvider.getDefaultKeyStoreOsPath()));
-                
-                
+                                mKeyStore));
+
                 DebugKeyProvider keyProvider = new DebugKeyProvider(
-                        null /* osKeyPath: use default */,
+                        mKeyStore /* Custom KeyStore */,
                         mStoreType, null /* IKeyGenOutput */);
                 PrivateKey key = keyProvider.getDebugKey();
                 X509Certificate certificate = (X509Certificate)keyProvider.getCertificate();
@@ -437,6 +446,7 @@ public final class ApkBuilder {
         System.err.println("    -v      Verbose.");
         System.err.println("    -u      Creates an unsigned package.");
         System.err.println("    -storetype Forces the KeyStore type. If ommited the default is used.");
+        System.err.println("    -keystore Uses a custom keystore (no password).");
         System.err.println("");
         System.err.println("    -z      Followed by the path to a zip archive.");
         System.err.println("            Adds the content of the application package.");
