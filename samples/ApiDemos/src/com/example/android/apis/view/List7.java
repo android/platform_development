96a1fbac144870b3a720fd1e66970fc4140f1739
Christian Mehlmauer <FireFart@gmail.com>
2010-05-20 21:40:17 +0200

    Replaced deprecated Contacts Api with ContactsContract
    Change-Id: I4816781c7b48b27ec64a63d51db02646c3d6c1a5

--- samples/ApiDemos/src/com/example/android/apis/view/List7.java
+++ samples/ApiDemos/src/com/example/android/apis/view/List7.java
@@ -20,10 +20,9 @@ package com.example.android.apis.view;
 // class is in a sub-package.
 import com.example.android.apis.R;
 
-
 import android.app.ListActivity;
 import android.database.Cursor;
-import android.provider.Contacts.People;
+import android.provider.ContactsContract;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
@@ -36,9 +35,22 @@ import android.widget.TextView;
  * A list view example where the data comes from a cursor.
  */
 public class List7 extends ListActivity implements OnItemSelectedListener {
-    private static String[] PROJECTION = new String[] {
-        People._ID, People.NAME, People.NUMBER
+    private static final String[] PEOPLE_PROJECTION = new String[] {
+        ContactsContract.Contacts._ID,
+        ContactsContract.Contacts.DISPLAY_NAME,
+        ContactsContract.Contacts.HAS_PHONE_NUMBER
     };
+
+    private static final String[] PHONE_PROJECTION = new String[] {
+        ContactsContract.CommonDataKinds.Phone._ID,
+        ContactsContract.CommonDataKinds.Phone.CONTACT_ID,
+        ContactsContract.CommonDataKinds.Phone.TYPE,
+        ContactsContract.CommonDataKinds.Phone.LABEL,
+        ContactsContract.CommonDataKinds.Phone.NUMBER
+    };
+
+    private int mColumnHasPhoneNumber;
+    private int mColumnContactId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,34 +60,70 @@ public class List7 extends ListActivity implements OnItemSelectedListener {
         getListView().setOnItemSelectedListener(this);
 
         // Get a cursor with all people
-        Cursor c = getContentResolver().query(People.CONTENT_URI, PROJECTION, null, null, null);
+        Cursor c = getContentResolver().query(ContactsContract.Contacts.CONTENT_URI,
+                PEOPLE_PROJECTION, null, null, null);
         startManagingCursor(c);
-        mPhoneColumnIndex = c.getColumnIndex(People.NUMBER);
+
+        mColumnHasPhoneNumber = c.getColumnIndex(ContactsContract.Contacts.HAS_PHONE_NUMBER);
+        mColumnContactId = c.getColumnIndex(ContactsContract.Contacts._ID);
 
         ListAdapter adapter = new SimpleCursorAdapter(this,
-                android.R.layout.simple_list_item_1, // Use a template
-                                                        // that displays a
-                                                        // text view
-                c, // Give the cursor to the list adatper
-                new String[] {People.NAME}, // Map the NAME column in the
-                                            // people database to...
-                new int[] {android.R.id.text1}); // The "text1" view defined in
-                                            // the XML template
+                // Use a template that displays a text view
+                android.R.layout.simple_list_item_1,
+                // Give the cursor to the list adatper
+                c,
+                // Map the DISPLAY_NAME column in the people database to...
+                new String[] {ContactsContract.Contacts.DISPLAY_NAME},
+                // The "text1" view defined in the XML template
+                new int[] {android.R.id.text1});
         setListAdapter(adapter);
     }
 
-    public void onItemSelected(AdapterView parent, View v, int position, long id) {
+    public void onItemSelected(AdapterView<?> parent, View v, int position, long id) {
         if (position >= 0) {
+            //Get current Contact cursor
             Cursor c = (Cursor) parent.getItemAtPosition(position);
-            mPhone.setText(c.getString(mPhoneColumnIndex));
+            int hasPhone = c.getInt(mColumnHasPhoneNumber);
+            String text = "Nothing...";
+            //Only if contact has a PhoneNumber
+            if (hasPhone == 1) {
+                int contactId = c.getInt(mColumnContactId);
+                //Filter the Phone cursor the the current Contact
+                Cursor phoneCursor = getContentResolver().query(
+                        ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
+                        PHONE_PROJECTION,
+                        ContactsContract.CommonDataKinds.Phone.CONTACT_ID +" = "+ contactId,
+                        null, null);
+                startManagingCursor(phoneCursor);
+                //Phone Number present?
+                if (phoneCursor.getCount() > 0) {
+                    //only display the first number
+                    phoneCursor.moveToFirst();
+                    int columnPhoneType = phoneCursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.TYPE);
+                    int columnPhoneNumber = phoneCursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER);
+                    int type = phoneCursor.getInt(columnPhoneType);
+                    String phone = phoneCursor.getString(columnPhoneNumber);
+                    String label = null;
+                    //Custom type? Then get the custom label
+                    if (type == ContactsContract.CommonDataKinds.Phone.TYPE_CUSTOM) {
+                        int columnPhoneLabel = phoneCursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.LABEL);
+                        label = phoneCursor.getString(columnPhoneLabel);
+                    }
+                    //Get the readable string
+                    String numberType = (String) ContactsContract.CommonDataKinds.Phone.getTypeLabel(
+                            getResources(), type, label);
+                    text = numberType + ": " + phone;
+                }
+                stopManagingCursor(phoneCursor);
+                phoneCursor.close();
+            }
+            mPhone.setText(text);
         }
     }
 
-    public void onNothingSelected(AdapterView parent) {
+    public void onNothingSelected(AdapterView<?> parent) {
         mPhone.setText(R.string.list_7_nothing);
-
     }
 
-    private int mPhoneColumnIndex;
     private TextView mPhone;
 }
