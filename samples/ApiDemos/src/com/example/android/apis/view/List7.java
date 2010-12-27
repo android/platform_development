3ab6a51af11bd0beb02888de8ebe5f8adf0eee47
kingmoon <kingmoon@gmail.com>
2010-12-27 09:41:32 +0800

    In API demo list 7, when use onItemSelectedListener, there is no effect when user click item on List7. So change it to onItemClickListener.
    Change-Id: I7cc5c63575197caf4e8a20cb29a0881d2152a409

--- samples/ApiDemos/src/com/example/android/apis/view/List7.java
+++ samples/ApiDemos/src/com/example/android/apis/view/List7.java
@@ -27,7 +27,7 @@ import android.provider.ContactsContract.CommonDataKinds.Phone;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ListAdapter;
 import android.widget.SimpleCursorAdapter;
 import android.widget.TextView;
@@ -35,7 +35,7 @@ import android.widget.TextView;
 /**
  * A list view example where the data comes from a cursor.
  */
-public class List7 extends ListActivity implements OnItemSelectedListener {
+public class List7 extends ListActivity implements OnItemClickListener {
 
     private TextView mPhone;
 
@@ -56,7 +56,7 @@ public class List7 extends ListActivity implements OnItemSelectedListener {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.list_7);
         mPhone = (TextView) findViewById(R.id.phone);
-        getListView().setOnItemSelectedListener(this);
+        getListView().setOnItemClickListener(this);
 
         // Get a cursor with all numbers.
         // This query will only return contacts with phone numbers
@@ -76,7 +76,7 @@ public class List7 extends ListActivity implements OnItemSelectedListener {
         setListAdapter(adapter);
     }
 
-    public void onItemSelected(AdapterView<?> parent, View v, int position, long id) {
+    public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
         if (position >= 0) {
             //Get current cursor
             Cursor c = (Cursor) parent.getItemAtPosition(position);
