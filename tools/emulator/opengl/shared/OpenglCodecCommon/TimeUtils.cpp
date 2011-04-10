3f9ffe198586c5c6a6e92bdfca4517496c8170d6
Guy Zadikario <guy@graphtech.co.il>
2011-04-10 17:40:02 +0300

    emulatgor opengl: First commit of the host renderer library.
    This is a library which includes all the OpenGL renderer functionality,
    it is packaged in a library so that both the renderer process and the emulator
    program will be able to use that functionality.
    
    NOTES:
       1) gl_proc.h and GLDispatch.{h,cpp} in this commit will be replaced
          with the decoder auto-generated dispatch in a later commit, the
          auto-generated dispatch is currently missing some extension functions
          required for the renderer.
       2) look at host/include/libOpenglRender/render_api.h for the external
          interface defined for this library (to be used by the emulator).
    
    The following is a description of each component:
    
    FrameBuffer - The main object which manages the framebuffer and color buffers.
                  This is a singleton which get initialized through its initialize
                  static function. It initializes the OpenGL renderer and must be
                  called first. This initialization function is not thread safe so
                  it must be called before any thread that is calling to this
                  library is created.
    
    FBConfig - Includes a static set of configs supported by the renderer which get
               initialized during FrameBuffer initialization phase. Also,
               an instance of this class includes the a description of one frame
               buffer configuration supported by the renderer.
    
    RenderContext - encapsulate a rendering context state.
    
    ColorBuffer - implements a color buffer object as a texture which can be bind
                  as render target or source.
    
    WindowSurface - implements the functionality of a native window which can be
                    bound to a rendering context and its target ColorBuffer can
                    be specified and replaced.
    
    ThreadInfo - holds per-thread information.
    
    EGLDispatch - loads the EGL plugin library, all egl calls are made through
                  this dispatch table which get initialized during initialization
                  phase.
    
    GLDispatch - loads the GLES plugin library, all GLES calls are made through
                 this dispatch table which get initialized during initialization
                 phase - This will be replaced by the auto-generated code of the
                 decoder ...
    
    RenderThread - implements a thread that reads command tokens from an IOStream
                   and decode it.
    
    RenderControl - implements the host side implementation of the renderControl
                    API, when a renderControl token is decoded from the stream
                    it is dispatched to this implementation.
    
    RenderServer - implements a TCP server which listens to port number and
                   launcges a RenderThread for each new connection.
    
    Change-Id: I9f34d17bdfcb715893a13cd30086c767f499df87

--- tools/emulator/opengl/shared/OpenglCodecCommon/TimeUtils.cpp
+++ tools/emulator/opengl/shared/OpenglCodecCommon/TimeUtils.cpp
@@ -23,6 +23,7 @@
 #include <stdlib.h>
 #include <sys/time.h>
 #include <time.h>
+#include <unistd.h>
 #else
 #include <sys/time.h>
 #endif
@@ -54,5 +55,14 @@ long long GetCurrentTimeMS()
     long long iDiff = (now.tv_sec * 1000LL) + now.tv_usec/1000LL;
     return iDiff;
 
+#endif
+}
+
+void TimeSleepMS(int p_mili)
+{
+#ifdef _WIN32
+    Sleep(p_mili);
+#else
+    usleep(p_mili * 1000);
 #endif
 }
