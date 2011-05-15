71aa7b14a1ff79c2e44068c424f5cbb7ee6b3d19
Stas Gurtovoy <stas@graphtech.co.il>
2011-05-15 11:29:56 +0300

    emulator opengl: added Bind texture to EGLImage/surface
    added functionality to bind an EGLImage to a texture both
    in the system and host sides.
    
    Change-Id: I2dc26090b45da080ac4d0e8f5799c95f7cc54634

--- tools/emulator/opengl/system/GLESv1_enc/GLEncoder.h
+++ tools/emulator/opengl/system/GLESv1_enc/GLEncoder.h
@@ -89,6 +89,8 @@ private:
     static void s_glPixelStorei(void *self, GLenum param, GLint value);
 
 	static void s_glFinish(void *self);
-    void sendVertexData(unsigned first, unsigned count);
+    static void s_glEGLImageTargetTexture2DOES(void * self, GLenum target, GLeglImageOES image);
+	void sendVertexData(unsigned first, unsigned count);
+	
 };
 #endif
