2ca89118b635fc3346f7689de49d0e83e17b1ee4
Bhanu Chetlapalli <bhanu@mips.com>
2012-04-09 15:24:30 -0700

    Explicitly include -X11 while building emulator_renderer
    emulator_renderer implicitly uses symbols defined by libX11.so
    through intermediate libraries, which can cause dependency issues
    if the intermediates drop the dependency. Systems like fedora now
    explicilty disable such indirect dependencies - which causes
    compilation failure.
    
    More information is available at this URL
    
    http://fedoraproject.org/wiki/UnderstandingDSOLinkChange
    
    Change-Id: I67f48814d27b91461d8652f3022b37a1145e292a
    Signed-Off-By: Bhanu Chetlapalli <bhanu@mips.com>

--- tools/emulator/opengl/host/renderer/Android.mk
+++ tools/emulator/opengl/host/renderer/Android.mk
@@ -1,5 +1,9 @@
 LOCAL_PATH:=$(call my-dir)
 
+ifeq ($(HOST_OS),linux)
+    $(call emugl-export,LDLIBS,-lX11)
+endif
+
 # host renderer process ###########################
 $(call emugl-begin-host-executable,emulator_renderer)
 $(call emugl-import,libOpenglRender)
