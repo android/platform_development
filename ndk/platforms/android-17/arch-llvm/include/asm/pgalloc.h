31a3e8dca95c33e7b409619df5932f06fa288cac
Shih-wei Liao <sliao@google.com>
2012-08-14 00:57:43 -0700

    arch-llvm headers to support NDK.
    Change-Id: Ia37264a3577d9d5714545d4b152012dde1e2e06d

---
+++ ndk/platforms/android-17/arch-llvm/include/asm/pgalloc.h
@@ -0,0 +1,21 @@
+/****************************************************************************
+ ****************************************************************************
+ ***
+ ***   This header was automatically generated from a Linux kernel header
+ ***   of the same name, to make information necessary for userspace to
+ ***   call into the kernel available to libc.  It contains only constants,
+ ***   structures, and macros generated from the original header, and thus,
+ ***   contains no copyrightable information.
+ ***
+ ****************************************************************************
+ ****************************************************************************/
+#ifndef _ASM_PGALLOC_H
+#define _ASM_PGALLOC_H
+
+#include <asm/processor.h>
+#include <asm/cacheflush.h>
+#include <asm/tlbflush.h>
+
+#define check_pgt_cache() do { } while (0)
+
+#endif
