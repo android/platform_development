eb7c12fdbe831626bda43000e63d59652e5dcfa1
Dan Willemsen <dwillemsen@google.com>
2015-08-25 16:24:01 -0700

    Stop checking HOST_OS
    Change-Id: I944f48aea13abef0efa157c82ef7c91a63429eb5

--- tools/etc1tool/Android.mk
+++ tools/etc1tool/Android.mk
@@ -21,17 +21,13 @@ LOCAL_STATIC_LIBRARIES := \
 	libpng \
 	libETC1
 
-ifeq ($(HOST_OS),linux)
-LOCAL_LDLIBS += -lrt
-endif
+LOCAL_LDLIBS_linux += -lrt
 
 # Statically link libz for MinGW (Win SDK under Linux),
 # and dynamically link for all others.
-ifneq ($(strip $(USE_MINGW)),)
-  LOCAL_STATIC_LIBRARIES += libz
-else
-  LOCAL_LDLIBS += -lz
-endif
+LOCAL_STATIC_LIBRARIES_windows += libz
+LOCAL_LDLIBS_darwin += -lz
+LOCAL_LDLIBS_linux += -lz
 
 LOCAL_MODULE := etc1tool
 
