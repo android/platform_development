378f499921a4a028ad95c483fb259c2dff545482
Yohann Roussel <yroussel@google.com>
2015-12-09 11:52:01 +0100

    Ensure Jack server is running before any use
    Change-Id: I6748c9c31e1df305c7dcc2568dbba50be4348e07

--- build/build_android_stubs.mk
+++ build/build_android_stubs.mk
@@ -51,5 +51,5 @@ $(full_target): $(stub_timestamp) $(framework_res_package)
 	$(hide) jar -cf $@ -C $(PRIVATE_CLASS_INTERMEDIATES_DIR) .
 	$(hide) jar -u0f $@ -C $(PRIVATE_CLASS_INTERMEDIATES_DIR) resources.arsc
 
-$(jack_lib) : $(full_target) $(JILL_JAR) $(JACK)
+$(jack_lib) : $(full_target) $(JILL_JAR) $(JACK) | setup-jack-server
 	$(transform-jar-to-jack)
