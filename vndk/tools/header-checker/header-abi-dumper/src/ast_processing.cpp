dd04ac2e6c8e0bbd7f368a95734c9b57d9d8f107
Jayant Chowdhary <jchowdhary@google.com>
2017-01-13 15:41:48 -0800

    Added libprotobuf to extract and dump ABI.
    Dump textual presentation of the ABI as well for
    ease of review.
    
    Test: First generate cpp protobuf files then test :
    
    aprotoc -I=header-abi-dumper/src/ --cpp_out=header-abi-dumper/src/
    header-abi-dumper/src/ABI.proto;
    
    mma -j64;
    
    header-abi-dumper -o example1.dump tests/example1.h -- clang -x
    c++ -I . -std=c++11.
    
    Change-Id: I65d90d1ec834d7c30cde4eb389862fd38a4e19e9

--- vndk/tools/header-checker/header-abi-dumper/src/ast_processing.cpp
+++ vndk/tools/header-checker/header-abi-dumper/src/ast_processing.cpp
@@ -1,31 +1,143 @@
 #include "ast_processing.h"
 
-bool HeaderASTVisitor::VisitRecordDecl(const clang::RecordDecl *decl) {
-  llvm::errs() << "struct: " << decl->getName() << "\n";
-  return true;
-}
+#include <clang/Tooling/Core/QualTypeNames.h>
+#include <google/protobuf/text_format.h>
 
-bool HeaderASTVisitor::VisitCXXRecordDecl(const clang::CXXRecordDecl *decl) {
-  llvm::errs() << "class: " << decl->getName() << "\n";
+#include <string>
+#include <fstream>
+#include <iostream>
+
+HeaderASTVisitor::HeaderASTVisitor(
+      src::TranslationUnit *TUPtr,
+      clang::MangleContext *mangle_contextp,
+      const clang::ASTContext *AST_contextp,
+      const clang::CompilerInstance *compiler_instance_p) :
+    TUp_(TUPtr),
+    mangle_contextp_(mangle_contextp),
+    AST_contextp_(AST_contextp),
+    cip_(compiler_instance_p)  {}
+
+bool HeaderASTVisitor::VisitRecordDecl(const clang::RecordDecl *decl) {
+  src::RecordDecl *record_decl = TUp_->add_classes();
+  SetupClassFields(record_decl, decl);
   return true;
 }
 
 bool HeaderASTVisitor::VisitFunctionDecl(const clang::FunctionDecl *decl) {
-  llvm::errs() << "func: " << decl->getName() << "\n";
+  src::FunctionDecl *function_decl = TUp_->add_functions();
+    //FIXME: use return value
+  SetupFunction(function_decl, decl);
   return true;
 }
 
+HeaderASTConsumer::HeaderASTConsumer(
+      std::string file_name,
+      clang::CompilerInstance *compiler_instancep,
+      std::string out_dump_name) :
+    file_name_(file_name),
+    cip_(compiler_instancep),
+    out_dump_name_(out_dump_name) {}
+
+
 void HeaderASTConsumer::HandleTranslationUnit(clang::ASTContext &ctx) {
-  llvm::errs() << "HandleTranslationUnit ------------------------------\n";
   clang::TranslationUnitDecl* translation_unit = ctx.getTranslationUnitDecl();
-  HeaderASTVisitor v;
+  std::unique_ptr<clang::MangleContext> mangle_contextp(
+      ctx.createMangleContext());
+  src::TranslationUnit TU;
+  HeaderASTVisitor v(&TU, mangle_contextp.get(), &ctx, cip_);
   v.TraverseDecl(translation_unit);
+  std::ofstream text_output(out_dump_name_ + ".txt");
+  std::fstream binary_output(
+      (out_dump_name_).c_str(),
+      std::ios::out | std::ios::trunc | std::ios::binary);
+  std::string str_out;
+  google::protobuf::TextFormat::PrintToString(TU, &str_out);
+  text_output << str_out;
+  if (!TU.SerializeToOstream(&binary_output)) {
+        llvm::errs() << "Serialization to ostream failed\n";
+  }
 }
 
 void HeaderASTConsumer::HandleVTable(clang::CXXRecordDecl *crd) {
   llvm::errs() << "HandleVTable: " << crd->getName() << "\n";
 }
 
+std::string HeaderASTVisitor::GetDeclSourceFile(
+    const clang::NamedDecl *decl) {
+  clang::SourceManager &SM = cip_->getSourceManager();
+  clang::SourceLocation location = decl->getLocation();
+  llvm::StringRef file_name= SM.getFilename(location);
+  return file_name.str();
+}
+
+std::string HeaderASTVisitor::GetMangledNameDecl(
+    const clang::NamedDecl *decl) {
+  std::string mangled_or_demangled_name = decl->getName();
+  if (mangle_contextp_->shouldMangleDeclName(decl)) {
+    llvm::raw_string_ostream ostream(mangled_or_demangled_name);
+    mangle_contextp_->mangleName(decl, ostream);
+    ostream.flush();
+  }
+  return mangled_or_demangled_name;
+}
+
+bool HeaderASTVisitor::SetupFunction(
+    src::FunctionDecl *functionp,
+    const clang::FunctionDecl *decl) {
+  // Go through all the parameters in the method
+  // and add them to the fields. Also get the fully qualfied
+  // name and mangled name and store them.
+  functionp->set_functionname(decl->getQualifiedNameAsString());
+  functionp->set_mangledfunctionname(GetMangledNameDecl(decl));
+  functionp->set_sourcefile(GetDeclSourceFile(decl));
+  clang::QualType return_type =
+      decl->getReturnType().getDesugaredType(*AST_contextp_);
+  functionp->set_returntype(
+      clang::TypeName::getFullyQualifiedName(return_type, *AST_contextp_));
+  clang::FunctionDecl::param_const_iterator paramit =
+      decl->param_begin();
+  src::FieldDecl *function_fieldp = nullptr;
+  while (paramit != decl->param_end()) {
+    if (!(function_fieldp = functionp->add_parameters())) {
+      llvm::errs() << "Couldn't add parameter to method. Aborting";
+      return false;
+    }
+    function_fieldp->set_fieldname((*paramit)->getName());
+    clang::QualType field_type =
+      (*paramit)->getType().getDesugaredType(*AST_contextp_);
+
+    function_fieldp->set_fieldtype(
+        clang::TypeName::getFullyQualifiedName(field_type, *AST_contextp_));
+
+    paramit++;
+  }
+  return true;
+}
+
+bool HeaderASTVisitor::SetupClassFields(
+    src::RecordDecl *classp,
+    const clang::RecordDecl *decl) {
+  classp->set_fullyqualifiedname(decl->getQualifiedNameAsString());
+  classp->set_sourcefile(GetDeclSourceFile(decl));
+  classp->set_entitytype("class");
+  src::FieldDecl *class_fieldp = nullptr;
+  clang::RecordDecl::field_iterator field = decl->field_begin();
+  while (field != decl->field_end()) {
+    if (!(class_fieldp = classp->add_fields())) {
+      llvm::errs() << " Couldn't add class field: " << field->getName()
+                   << " to reference dump\n";
+      return false;
+    }
+    class_fieldp->set_fieldname(field->getName());
+    //FIXME: This needs to change. Resolve typedef, class name, built-in etc.
+    clang::QualType field_type =
+      field->getType().getDesugaredType(*AST_contextp_);
+    class_fieldp->set_fieldtype(
+        clang::TypeName::getFullyQualifiedName(field_type, *AST_contextp_));
+    field++;
+  }
+  return true;
+}
 llvm::StringRef HeaderASTPPCallbacks::ToString(const clang::Token &tok) {
   return tok.getIdentifierInfo()->getName();
 }
@@ -33,5 +145,4 @@ llvm::StringRef HeaderASTPPCallbacks::ToString(const clang::Token &tok) {
 void HeaderASTPPCallbacks::MacroDefined(const clang::Token &macro_name_tok,
     const clang::MacroDirective *) {
   assert(macro_name_tok.isAnyIdentifier());
-  llvm::errs() << "defines: " << ToString(macro_name_tok) << "\n";
 }
