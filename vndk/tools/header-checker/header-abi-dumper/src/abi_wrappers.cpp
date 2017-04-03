79be0356a99365a4f44def1b1e68250239b0cb99
Jayant Chowdhary <jchowdhary@google.com>
2017-04-03 14:33:43 -0700

    Add option to parse stub files during linking.
    1) Add option to parse stub files specifying exported symbols and
    variables during link time.
    2) fixed mangled names of global variables : name doesn't appear in
    addition to mangled name.
    3) Added test stub file.
    
    Test: 1) header-abi-dumper -o example1.dump tests/input/example1.cpp -I
    tests -- -x c++ -std=c++11 -I . -I
    ~/android/aosp/external/clang/lib/Headers
    
    2) header-abi-linker -o test.lsdump example1.dump -s
    tests/input/test_stub.c, test.lsdump has only the symbols specified by
    test_stub.c
    
    Change-Id: Ib539db4a886abe4917e09cd8643ab35ffecb0df1

--- vndk/tools/header-checker/header-abi-dumper/src/abi_wrappers.cpp
+++ vndk/tools/header-checker/header-abi-dumper/src/abi_wrappers.cpp
@@ -101,6 +101,7 @@ std::string ABIWrapper::GetMangledNameDecl(const clang::NamedDecl *decl) const {
   std::string mangled_or_demangled_name =
       identifier ? identifier->getName() : "";
   if (mangle_contextp_->shouldMangleDeclName(decl)) {
+    mangled_or_demangled_name = "";
     llvm::raw_string_ostream ostream(mangled_or_demangled_name);
     mangle_contextp_->mangleName(decl, ostream);
     ostream.flush();
