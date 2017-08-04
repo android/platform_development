0b7fea105738f45215202cc8bea7aa24c5dea70a
Fan-yun Sun <fanyun@google.com>
2017-08-04 16:13:49 +0800

    Modify project structure
    Test: ok
    Change-Id: If23263349a85478be2e72c3c326e2ba774a0dc4c

---
+++ vndk/tools/source-deps-reviewer/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='sourcedr',
+    version='1.0',
+    description='Shared Libs Deps Review Tool',
+    url='https://android.googlesource.com/platform/development/+/master/vndk/tools/source-deps-reviewer/',
+    author='Fan-Yun Sun',
+    packages=['sourcedr'],
+    install_requires=['flask'],
+    extras_require={
+        'dev': [
+            'flask_testing'
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            'sourcedr = sourcedr.server:main',
+        ],
+    }
+)
