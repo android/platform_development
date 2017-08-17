be69fa9e6571d2c2e7c2334ebabea8f036f0c0a9
Fan-yun Sun <fanyun@google.com>
2017-08-17 16:18:17 +0800

    Add new features and reorganize code
    Test: ./sourcedr/functional_tests.py
    Change-Id: I805ffda3068ca8f82300f6d479683afe1e004973

--- vndk/tools/source-deps-reviewer/sourcedr/test/example.c
+++
@@ -1,9 +0,0 @@
-int main() {
-    printf("This is a simple testing file\n");
-    "This line with dlopen shouldn't be found"
-    'This line with dlopen
-     shouldn't be found'
-    /*
-     * This dlopen shouldn't be found
-     */
-}
