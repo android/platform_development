6868733440c6c743888ad16b06d016cfbcedc856
Jae Shin <jaeshin@google.com>
2017-12-29 13:24:54 +0900

    Generate separate Android.bp per snapshot variant
    VNDK snapshot now packages not only the primary abi but also the
    secondary abi for each build target. To distinguish 32bit srcs
    built from 64bit vs 32bit TARGET_ARCHs, each prebuilt is defined
    per TARGET_ARCH and included in corresponding phony package,
    vndk_v$VER_$ARCH.
    
    Test: python update.py [options]
    Bug: 71370248
    Change-Id: I1be1d4421b97855a95be6ca0b8efb62e50205a7d

--- vndk/snapshot/update.py
+++ vndk/snapshot/update.py
@@ -100,12 +100,12 @@ def install_snapshot(branch, build, install_dir):
             fetch_artifact(branch, build, artifact_pattern)
 
             manifest_pattern = 'manifest_{}.xml'.format(build)
-            manifest_name = 'manifest.xml'
+            manifest_name = utils.MANIFEST_FILE_NAME
             logger().info(
                 'Fetching {file} from {branch} (bid: {build})'.format(
                     file=manifest_pattern, branch=branch, build=build))
             fetch_artifact(branch, build, manifest_pattern, manifest_name)
-            shutil.move(manifest_name, install_dir)
+            shutil.move(manifest_name, utils.COMMON_DIR_PATH)
 
             os.chdir(install_dir)
         else:
@@ -128,25 +128,23 @@ def install_snapshot(branch, build, install_dir):
             shutil.rmtree(tempdir)
 
 
-def gather_notice_files():
-    """Gathers all NOTICE files to a new NOTICE_FILES directory.
+def gather_notice_files(install_dir):
+    """Gathers all NOTICE files to a common NOTICE_FILES directory."""
 
-    Create a new NOTICE_FILES directory under install_dir and copy to it
-    all NOTICE files in arch-*/NOTICE_FILES.
-    """
-    notices_dir_name = 'NOTICE_FILES'
-    logger().info('Creating {} directory...'.format(notices_dir_name))
-    os.makedirs(notices_dir_name)
-    for arch_dir in glob.glob('arch-*'):
-        notices_dir_per_arch = os.path.join(arch_dir, notices_dir_name)
-        if os.path.isdir(notices_dir_per_arch):
+    common_notices_dir = utils.NOTICE_FILES_DIR_PATH
+    logger().info('Creating {} directory...'.format(common_notices_dir))
+    os.makedirs(common_notices_dir)
+    for variant in utils.get_snapshot_variants(install_dir):
+        notices_dir_per_variant = os.path.join(variant,
+                                               utils.NOTICE_FILES_DIR_NAME)
+        if os.path.isdir(notices_dir_per_variant):
             for notice_file in glob.glob(
-                    '{}/*.txt'.format(notices_dir_per_arch)):
+                    '{}/*.txt'.format(notices_dir_per_variant)):
                 if not os.path.isfile(
-                        os.path.join(notices_dir_name,
+                        os.path.join(common_notices_dir,
                                      os.path.basename(notice_file))):
-                    shutil.copy(notice_file, notices_dir_name)
-            shutil.rmtree(notices_dir_per_arch)
+                    shutil.copy(notice_file, common_notices_dir)
+            shutil.rmtree(notices_dir_per_variant)
 
 
 def revise_ld_config_txt():
@@ -166,10 +164,10 @@ def revise_ld_config_txt():
 
 
 def update_buildfiles(buildfile_generator):
-    logger().info('Updating Android.mk...')
+    logger().info('Generating Android.mk file...')
     buildfile_generator.generate_android_mk()
 
-    logger().info('Updating Android.bp...')
+    logger().info('Generating Android.bp files...')
     buildfile_generator.generate_android_bp()
 
 
@@ -251,7 +249,7 @@ def main():
 
     remove_old_snapshot(install_dir)
     install_snapshot(args.branch, args.build, install_dir)
-    gather_notice_files()
+    gather_notice_files(install_dir)
 
     # Post-process ld.config.txt for O-MR1
     if vndk_version == '27':
