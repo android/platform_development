0265395ac2ebb21834cec66ac5dcac7780fd94df
Bowgo Tsai <bowgotsai@google.com>
2018-07-17 18:06:17 +0800

    Move SdkSetup.apk to vendor partition
    Otherwise, it causes the CTS-on-GSI failure on non-emulator devices:
      CtsPermission2TestCases android.permission2.cts.PrivappPermissionsTest#testPrivappPermissionsEnforcement
    
    Because non-emulator devices lack the permission file in their /vendor.
      /system/priv-app/SdkSetup/SdkSetup.apk
      /vendor/etc/permissions/privapp-permissions-goldfish.xml
    
    Also need to remove LOCAL_PRIVATE_PLATFORM_APIS because vendor apk
    can only use SDK APIs.
    
    Bug: 110517983
    Test: boot a GSI and checks that SdkSetup.apk is no longer in /system.
    
    Change-Id: Idc4d54806fbcc78cb1b2fdbc55bb78a76fa08bc1

--- apps/SdkSetup/Android.mk
+++ apps/SdkSetup/Android.mk
@@ -3,10 +3,10 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE_TAGS := optional
 LOCAL_PRIVILEGED_MODULE := true
+LOCAL_VENDOR_MODULE := true
 LOCAL_SRC_FILES := $(call all-subdir-java-files)
 
 LOCAL_PACKAGE_NAME := SdkSetup
-LOCAL_PRIVATE_PLATFORM_APIS := true
 LOCAL_CERTIFICATE := platform
 
 LOCAL_PROGUARD_ENABLED := disabled
