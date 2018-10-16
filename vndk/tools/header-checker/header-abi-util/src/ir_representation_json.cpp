4c418a695e571de3cfbdb34e015d4b1243fc27b8
Logan Chien <loganchien@google.com>
2018-10-16 11:45:41 +0800

    header-checker: Code cleanup
    This commit cleans up several whitespaces nits, re-indent the code,
    remove unnecessary data member initializers, and add missing header
    guards.
    
    Test: ./tests/test.py
    Change-Id: Ibc355441aaa110280b263c5c3d8fc766941b08af

--- vndk/tools/header-checker/header-abi-util/src/ir_representation_json.cpp
+++ vndk/tools/header-checker/header-abi-util/src/ir_representation_json.cpp
@@ -12,10 +12,11 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include <ir_representation_json.h>
+#include "ir_representation_json.h"
 
 #include <json/reader.h>
 #include <json/writer.h>
+
 #include <llvm/Support/raw_ostream.h>
 
 #include <cstdlib>
