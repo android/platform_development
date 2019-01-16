ed2c4121c3be851233d9141b3405412b0573cd49
Logan Chien <loganchien@google.com>
2019-01-17 00:23:51 +0800

    header-abi-linker: Support solib and version script
    This commit allows user to specify both `-so` (for a shared object file)
    and `-v` (for a version script).  If both of them are specified, the
    generated output will only include the symbols that are marked as
    exported in both cases.
    
    This commit also rewritten the version script parser so that it can
    work with real version script cases.  This includes how the parser
    skips a comment line.
    
    Furthermore, the version script parser accepts a list of excluded symbol
    versions and symbol tags that should not be included in the generated
    output.  To customize the behavior, pass `--exclude-symbol-version`
    and/or `--exclude-symbol-tag` to `header-abi-linker`.
    
    Bug: 122845490
    Test: ./tests/test.py
    Test: out/host/linux-x86/nativetest64/libheader-abi-util_test/\
          libheader-abi-util_test
    Change-Id: Ibf048bba6ccd5ac44f265d1fe767c2ded3d13a8d

---
+++ vndk/tools/header-checker/header-abi-util/src/exported_symbol_set.cpp
@@ -0,0 +1,93 @@
+// Copyright (C) 2019 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "exported_symbol_set.h"
+
+#include "ir_representation.h"
+#include "stl_utils.h"
+#include "string_utils.h"
+
+#include <fnmatch.h>
+#include <cxxabi.h>
+
+
+namespace abi_util {
+
+
+static inline bool IsCppSymbol(const std::string &name) {
+  return StartsWith(name, "_Z");
+}
+
+
+static inline bool HasMatchingGlobPattern(
+    const ExportedSymbolSet::GlobPatternSet &patterns,
+    const std::string &name) {
+  for (auto &&pattern : patterns) {
+    if (fnmatch(pattern.c_str(), name.c_str(), 0) == 0) {
+      return true;
+    }
+  }
+  return false;
+}
+
+
+void ExportedSymbolSet::AddFunction(const std::string &name,
+                                    ElfSymbolIR::ElfSymbolBinding binding) {
+  funcs_.emplace(name, ElfFunctionIR(name, binding));
+}
+
+
+void ExportedSymbolSet::AddVar(const std::string &name,
+                               ElfSymbolIR::ElfSymbolBinding binding) {
+  vars_.emplace(name, ElfObjectIR(name, binding));
+}
+
+
+bool ExportedSymbolSet::HasSymbol(const std::string &name) const {
+  if (funcs_.find(name) != funcs_.end()) {
+    return true;
+  }
+
+  if (vars_.find(name) != vars_.end()) {
+    return true;
+  }
+
+  if (HasMatchingGlobPattern(glob_patterns_, name)) {
+    return true;
+  }
+
+  if (IsCppSymbol(name)) {
+    std::unique_ptr<char, FreeDeleter> demangled_name_c_str(
+        abi::__cxa_demangle(name.c_str(), nullptr, nullptr, nullptr));
+
+    if (demangled_name_c_str) {
+      std::string demangled_name(demangled_name_c_str.get());
+
+      if (demangled_cpp_symbols_.find(demangled_name) !=
+          demangled_cpp_symbols_.end()) {
+        return true;
+      }
+
+      if (HasMatchingGlobPattern(demangled_cpp_glob_patterns_,
+                                 demangled_name)) {
+        return true;
+      }
+    }
+  }
+
+  return false;
+}
+
+
+}  // namespace abi_util
