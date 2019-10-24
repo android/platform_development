013bf1db81330e3ad3d44db90948432500e2443f
Hsin-Yi Chen <hsinyichen@google.com>
2019-10-24 11:19:35 +0800

    Include anonymous enum in ABI dumps
    Bug: 141709599
    Test: ./test.py
    Change-Id: I557c016fab57c0587feb6be7f72cfcd0abf90b28

---
+++ vndk/tools/header-checker/tests/integration/cpp/anonymous_enum/include/swap_enum.h
@@ -0,0 +1,11 @@
+struct {
+  enum {
+    B,
+    C,
+  } member;
+} global_var;
+
+enum : char {
+  A=1,
+};
+
---
+++ vndk/tools/header-checker/tests/integration/cpp/anonymous_enum/include/swap_enum_field.h
@@ -0,0 +1,11 @@
+enum : char {
+  A=1,
+};
+
+struct {
+  enum {
+    C,
+    B,
+  } member;
+} global_var;
+
