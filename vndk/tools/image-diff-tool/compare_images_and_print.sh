1e236ea605442f93a4642badc1a9e55dc422ae85
Yo Chiang <yochiang@google.com>
2019-11-11 17:07:45 +0800

    Add 'whitelist' option to compare_images
    * `compare_images -t a b -s system -w whitelist.txt` to specify
      whitelist.
    * diff.csv is split into two parts:
      - whitelisted_diff.csv: Diffs that are ignored by a whitelist
      - diff.csv: Diffs that are not ignored by any whitelist.
    * compare_images_and_print.sh returns error when there is any
      unwhitelisted diffs.
    
    Test: Tested manually by creating two directories that contain common
          files, whitelisted diffing files and not whitelisted diffing files.
    Test: compare_images -t a b -s SYSTEM -i -w whitelist.txt
    Change-Id: Ie80741d08ef5bc245b409b46ee716ae783428a7d

--- vndk/tools/image-diff-tool/compare_images_and_print.sh
+++ vndk/tools/image-diff-tool/compare_images_and_print.sh
@@ -1,14 +1,27 @@
 #!/bin/bash
-build/soong/soong_ui.bash --make-mode compare_images
+
+build/soong/soong_ui.bash --make-mode --skip-make compare_images
 . build/envsetup.sh
 lunch aosp_arm64
 out/host/linux-x86/bin/compare_images $1 -u
+echo
 echo " - Common parts"
 cat common.csv
 echo
-echo " - Different parts"
+echo " - Different parts (that are whitelisted)"
+cat whitelisted_diff.csv
+echo
+echo " - Different parts (that are not whitelisted)"
 cat diff.csv
 
 if [ -v DIST_DIR ]; then
-  cp common.csv diff.csv $DIST_DIR
+  cp common.csv diff.csv whitelisted_diff.csv $DIST_DIR
+fi
+
+if [ "$(wc -l diff.csv | cut -d' ' -f1)" -gt "1" ]; then
+  echo "" >&2
+  echo "[ERROR] Unexpected diffing files" >&2
+  echo "There are diffing files that are not ignored by whitelist." >&2
+  echo "The whitelist may need to be updated." >&2
+  exit 1
 fi
