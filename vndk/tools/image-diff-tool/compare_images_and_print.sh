818075cc4c197b43c057f2ac64c4f9994c88b295
Yo Chiang <yochiang@google.com>
2019-11-11 17:07:45 +0800

    Add 'whitelist' option to compare_images
    * `compare_images -t a b -s system -w whitelist.txt` to specify
      whitelist.
    * diff.csv is split into two parts:
      - whitelisted_diff.csv: Diffs that are ignored by a whitelist
      - diff.csv: Diffs that are not ignored by any whitelist.
    * Add `-c, --check` option to compare_images_and_print.sh
      When this option is specified, compare_images_and_print.sh exits with
      a non-zero status if diff.csv contains any unexpected diffs.
    
    Bug: 144398662
    Test: Tested manually by creating two directories that contain common
          files, whitelisted diffing files and not whitelisted diffing files.
    Test: ./compare_images_and_print.sh --help
    Test: ./compare_images_and_print.sh --check "-t a b -s SYSTEM -i"
    Change-Id: Ie80741d08ef5bc245b409b46ee716ae783428a7d

--- vndk/tools/image-diff-tool/compare_images_and_print.sh
+++ vndk/tools/image-diff-tool/compare_images_and_print.sh
@@ -1,14 +1,69 @@
 #!/bin/bash
+
+set -e
+
+PROGNAME="$0"
+
+function usage {
+  cat <<EOF
+Usage:
+    ${PROGNAME} [OPTION] ... args_for_compare_images
+
+Options:
+    -h, --help
+        Show this message and exit.
+    -c, --check
+        Exit with a non-zero status if there is any unexpected diffs.
+EOF
+}
+
+while [[ "$1" =~ ^- ]] && [[ "$1" != "--" ]]; do
+  case "$1" in
+    -h | --help)
+      usage
+      exit 0
+      ;;
+    -c | --check)
+      CHECK_DIFF_RESULT=1
+      ;;
+    *)
+      break
+      ;;
+  esac
+  shift
+done
+if [[ "$1" == "--" ]]; then
+  shift
+fi
+
+readonly CHECK_DIFF_RESULT
+
 build/soong/soong_ui.bash --make-mode compare_images
 . build/envsetup.sh
 lunch aosp_arm64
-out/host/linux-x86/bin/compare_images $1 -u
+COMMON_WHITELIST=development/vndk/tools/image-diff-tool/whitelist.txt
+out/host/linux-x86/bin/compare_images $1 -u -w "${COMMON_WHITELIST}"
+
+echo
 echo " - Common parts"
 cat common.csv
 echo
-echo " - Different parts"
+echo " - Different parts (that are whitelisted)"
+cat whitelisted_diff.csv
+echo
+echo " - Different parts (that are not whitelisted)"
 cat diff.csv
 
 if [ -v DIST_DIR ]; then
-  cp common.csv diff.csv $DIST_DIR
+  cp common.csv diff.csv whitelisted_diff.csv $DIST_DIR
+fi
+
+if [ "$(wc -l diff.csv | cut -d' ' -f1)" -gt "1" ]; then
+  echo "" >&2
+  echo "[ERROR] Unexpected diffing files" >&2
+  echo "There are diffing files that are not ignored by whitelist." >&2
+  echo "The whitelist may need to be updated." >&2
+  if [ -n "${CHECK_DIFF_RESULT}" ]; then
+    exit 1
+  fi
 fi
