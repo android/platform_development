fdeb3ca6f8b1f97e9477e8c98dd29807e910f731
Hsin-Yi Chen <hsinyichen@google.com>
2020-08-26 17:55:04 +0800

    Merge ModuleIR with single thread
    This commit moves the ModuleMerger::MergeGraphs function calls from the
    reader threads to the main thread. The dumps are merged to the final
    ModuleIR in a deterministic order.
    
    Test: development/vndk/tools/header-checker/tests/test.py
    Bug: 162201277
    Change-Id: I9f6ea5bcbef0c74ff4c87b825bfec5ef02575ab2

--- vndk/tools/header-checker/src/linker/header_abi_linker.cpp
+++ vndk/tools/header-checker/src/linker/header_abi_linker.cpp
@@ -191,61 +191,50 @@ class HeaderAbiLinker {
   std::unique_ptr<repr::ExportedSymbolSet> version_script_symbols_;
 };
 
-static void
-DeDuplicateAbiElementsThread(const std::vector<std::string> &dump_files,
-                             const std::set<std::string> *exported_headers,
-                             linker::ModuleMerger *global_merger,
-                             std::mutex *global_merger_lock,
-                             std::atomic<std::size_t> *cnt) {
-  linker::ModuleMerger local_merger(exported_headers);
-
-  auto begin_it = dump_files.begin();
-  std::size_t num_sources = dump_files.size();
-  while (1) {
-    std::size_t i = cnt->fetch_add(sources_per_thread);
-    if (i >= num_sources) {
-      break;
+static void DeDuplicateAbiElementsThread(
+    std::vector<std::string>::const_iterator dump_files_begin,
+    std::vector<std::string>::const_iterator dump_files_end,
+    const std::set<std::string> *exported_headers,
+    linker::ModuleMerger *merger) {
+  for (auto it = dump_files_begin; it != dump_files_end; it++) {
+    std::unique_ptr<repr::IRReader> reader =
+        repr::IRReader::CreateIRReader(input_format, exported_headers);
+    assert(reader != nullptr);
+    if (!reader->ReadDump(*it)) {
+      llvm::errs() << "ReadDump failed\n";
+      ::exit(1);
     }
-    std::size_t end = std::min(i + sources_per_thread, num_sources);
-    for (auto it = begin_it + i; it != begin_it + end; it++) {
-      std::unique_ptr<repr::IRReader> reader =
-          repr::IRReader::CreateIRReader(input_format, exported_headers);
-      assert(reader != nullptr);
-      if (!reader->ReadDump(*it)) {
-        llvm::errs() << "ReadDump failed\n";
-        ::exit(1);
-      }
-      local_merger.MergeGraphs(reader->GetModule());
-    }
+    merger->MergeGraphs(reader->GetModule());
   }
-
-  std::lock_guard<std::mutex> lock(*global_merger_lock);
-  global_merger->MergeGraphs(local_merger.GetModule());
 }
 
 std::unique_ptr<linker::ModuleMerger> HeaderAbiLinker::ReadInputDumpFiles() {
-  std::unique_ptr<linker::ModuleMerger> merger(
-      new linker::ModuleMerger(&exported_headers_));
-
   std::size_t max_threads = std::thread::hardware_concurrency();
-  std::size_t num_threads =
-      sources_per_thread < dump_files_.size()
-          ? std::min(dump_files_.size() / sources_per_thread, max_threads)
-          : 1;
+  std::size_t num_threads = std::max<std::size_t>(
+      std::min(dump_files_.size() / sources_per_thread, max_threads), 1);
   std::vector<std::thread> threads;
-  std::atomic<std::size_t> cnt(0);
-  std::mutex merger_lock;
-  for (std::size_t i = 1; i < num_threads; i++) {
-    threads.emplace_back(DeDuplicateAbiElementsThread, dump_files_,
-                         &exported_headers_, merger.get(), &merger_lock, &cnt);
+  std::vector<std::unique_ptr<linker::ModuleMerger>> mergers;
+
+  std::size_t dump_files_index = 0;
+  for (std::size_t i = 0; i < num_threads; i++) {
+    std::size_t cnt = dump_files_.size() / num_threads +
+                      (i < dump_files_.size() % num_threads ? 1 : 0);
+    mergers.emplace_back(new linker::ModuleMerger(&exported_headers_));
+    threads.emplace_back(DeDuplicateAbiElementsThread,
+                         dump_files_.begin() + dump_files_index,
+                         dump_files_.begin() + dump_files_index + cnt,
+                         &exported_headers_, mergers.back().get());
+    dump_files_index += cnt;
   }
-  DeDuplicateAbiElementsThread(dump_files_, &exported_headers_, merger.get(),
-                               &merger_lock, &cnt);
-  for (auto &thread : threads) {
-    thread.join();
+  assert(dump_files_index == dump_files_.size());
+
+  threads[0].join();
+  for (std::size_t i = 1; i < threads.size(); i++) {
+    threads[i].join();
+    mergers[0]->MergeGraphs(mergers[i]->GetModule());
   }
 
-  return merger;
+  return std::move(mergers[0]);
 }
 
 bool HeaderAbiLinker::LinkAndDump() {
