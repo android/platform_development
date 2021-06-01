929eb4c6ad87f1f2e9bfae79be7419151632ebbd
lishutong <lishutong@google.com>
2021-06-01 18:57:44 +0000

    A demo of OTAGUI, still in its very early version. Functions: - Create OTA_package generate jobs. - Monitor OTA_package generate jobs. Please refer to 'README.md' for more details. Test: Mannual tested.
    Change-Id: Ie0eb836e28b338b50c58abe146a234dfb751e163

---
+++ tools/otagui/src/views/JobDetails.vue
@@ -0,0 +1,29 @@
+<template>
+  <div v-if="job">
+    <h1>{{ job.title }}</h1>
+    <p>{{ job.time }} on {{ job.date }} @ {{ job.location }}</p>
+    <p>{{ job.description }}</p>
+  </div>
+</template>
+
+<script>
+import ApiService from '../services/ApiService.js'
+export default {
+  props: ['id'],
+  data() {
+    return {
+      job: null,
+    }
+  },
+  created() {
+    // fetch job (by id) and set local job data
+    ApiService.getjob(this.id)
+      .then((response) => {
+        this.job = response.data
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  },
+}
+</script>
---
+++ tools/otagui/src/views/JobList.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="jobs">
+    <JobDisplay
+      v-for="job in jobs"
+      :key="job.id"
+      :job="job"
+    />
+    <button @click="updateStatus">
+      Update
+    </button>
+  </div>
+</template>
+
+<script>
+import JobDisplay from '@/components/JobDisplay.vue'
+import ApiService from '../services/ApiService.js'
+
+export default {
+  name: 'JobList',
+  components: {
+    JobDisplay,
+  },
+  data() {
+    return {
+      jobs: null,
+    }
+  },
+  created (){
+    this.updateStatus()
+  },
+  methods:{
+    updateStatus() {
+      ApiService.getJobs()
+        .then((response) => {
+          console.log(response.data)
+          this.jobs = response.data
+        })
+        .catch((err) => {
+          console.log(err)
+        })
+    }
+  }
+}
+
+</script>
+
+<style scoped>
+.jobs {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+</style>
