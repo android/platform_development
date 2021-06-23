662ea6477a74e82cc22360737ca3aa0e64a54a0a
Dan Willemsen <dwillemsen@google.com>
2021-06-22 18:14:53 -0700

    Split the sdk-repo platform-tools out of the main sdk build
    Right now, you have to build the full legacy SDK package in order to
    extract the different sdk-repo packages. As we're deprecating support
    for building the system image on Mac, we'll no longer be able to build
    the full system image there. It's also a bit wasteful for the Windows
    SDK, which only needs to extract a small portion of the SDK.
    
    So instead of trying to make the legacy SDK package smaller, move the
    platform-tools package build into it's own make logic, not using atree.
    A future change will split build-tools as well.
    
    Bug: 187222815
    Test: check that the linux/mac/windows sdk-repo-...-platform-tools-...zip
      files contain the same files
    Change-Id: I7773b321fddfc478472837d5f91f02b3b16f0bb5

---
+++ build/sdk_repo.mk
@@ -0,0 +1,64 @@
+# Inputs:
+#   my_prefix -- HOST_ / HOST_CROSS_
+#   LOCAL_NAME -- Name of the sdk_repo (e.g. platform-tools)
+#   LOCAL_PREFIX_DIR -- The name of the directory to use within the zip file
+#   LOCAL_FILES -- src:dest pairs of files to copy into the sdk repo.
+#   LOCAL_STRIP_FILES -- src:dest pairs of files to copy into the sdk repo, then strip
+#   LOCAL_DIRS -- src:dest pairs of directories to copy into the sdk repo
+#
+# Any of the src:dest pairs above can just be a source file name if the destination should just be
+# the basename of the source file.
+
+intermediates := $(call intermediates-dir-for,PACKAGING,sdk_repo_$(LOCAL_NAME),$(patsubst %_,%,$(my_prefix)))
+my_zipfile := $(intermediates)/sdk-repo-$($(my_prefix)OS)-$(LOCAL_NAME).zip
+
+# Add any missing destinations
+sdk_repo_expand = $(foreach p,$(1),$(if $(findstring :,$(p)),$(p),$(p):$(notdir $(p))))
+LOCAL_FILES := $(call sdk_repo_expand,$(LOCAL_FILES))
+LOCAL_STRIP_FILES := $(call sdk_repo_expand,$(LOCAL_STRIP_FILES))
+LOCAL_DIRS := $(call sdk_repo_expand,$(LOCAL_DIRS))
+sdk_repo_expand :=
+
+# Combine LOCAL_FILES and LOCAL_STRIP_FILES, as we need to copy both.
+LOCAL_FILES := $(sort $(LOCAL_FILES) $(LOCAL_STRIP_FILES))
+LOCAL_STRIP_FILES := $(sort $(foreach p,$(LOCAL_STRIP_FILES),$(call word-colon,2,$(p))))
+
+.PHONY: sdk_repo_$(LOCAL_NAME)
+sdk_repo_$(LOCAL_NAME): $(my_zipfile)
+$(call dist-for-goals,sdk_repo sdk_repo_$(LOCAL_NAME),$(my_zipfile):sdk-repo-$($(my_prefix)OS)-$(LOCAL_NAME)-$(FILE_NAME_TAG).zip)
+
+my_deps := \
+  $(foreach pair,$(LOCAL_FILES),$(call word-colon,1,$(pair))) \
+  $(foreach pair,$(LOCAL_DIRS),$(sort $(patsubst ./%,%,$(shell find -L $(call word-colon,1,$(pair))))))
+
+$(my_zipfile): PRIVATE_TMPDIR := $(intermediates)/tmp
+$(my_zipfile): PRIVATE_DESTDIR := $(intermediates)/tmp/$(LOCAL_PREFIX_DIR)
+$(my_zipfile): PRIVATE_PARENT_DIRS := \
+  $(sort $(foreach pair,$(LOCAL_FILES) $(LOCAL_DIRS),\
+    $(dir $(call word-colon,2,$(pair)))))
+$(my_zipfile): PRIVATE_FILES := $(LOCAL_FILES)
+$(my_zipfile): PRIVATE_STRIP_FILES := $(LOCAL_STRIP_FILES)
+$(my_zipfile): PRIVATE_DIRS := $(LOCAL_DIRS)
+$(my_zipfile): $(SOONG_ZIP) $(HOST_STRIP) $(my_deps)
+	rm -rf $@ $(PRIVATE_TMPDIR)
+	mkdir -p $(PRIVATE_DESTDIR)
+	$(foreach d,$(PRIVATE_PARENT_DIRS),mkdir -p $(PRIVATE_DESTDIR)/$(d) && ) true
+	$(foreach f,$(PRIVATE_FILES),\
+	  cp $(call word-colon,1,$(f)) $(PRIVATE_DESTDIR)/$(call word-colon,2,$(f)) && ) true
+	$(foreach d,$(PRIVATE_DIRS),\
+	  cp -R $(call word-colon,1,$(d)) $(PRIVATE_DESTDIR)/$(call word-colon,2,$(d)) && ) true
+	# TODO: Handle more sdk excluded files (sdk.exclude.atree)
+	find $(PRIVATE_DESTDIR) '(' -name '.*' -o -name '*~' ')' | xargs rm -rf
+	$(foreach f,$(PRIVATE_STRIP_FILES),\
+	  $(HOST_STRIP) -x $(PRIVATE_DESTDIR)/$(f))
+	$(SOONG_ZIP) -C $(PRIVATE_TMPDIR) -D $(PRIVATE_TMPDIR) -o $@
+
+# Clear used variables
+my_prefix :=
+my_deps :=
+my_zipfile :=
+LOCAL_NAME :=
+LOCAL_PREFIX_DIR :=
+LOCAL_FILES :=
+LOCAL_STRIP_FILES :=
+LOCAL_DIRS :=
