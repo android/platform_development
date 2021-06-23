590f9ce843bfa9e3e673cd1ed05149c31a9449b8
Dan Willemsen <dwillemsen@google.com>
2021-06-22 18:14:53 -0700

    Split the sdk-repo platform-tools out of the main sdk build
    Right now, you have to build the full legacy SDK package in order to
    extract the different sdk-repo packages. As we're deprecating support
    for building the system image on Mac, we'll no longer be able to build
    the full system image there. It's also a bit wasteful for the Windows
    SDK, which only needs to extract a small portion of the SDK.
    
    So instead of trying to make the legacy SDK package smaller, move the
    platform-tools package build into it's own make logic, not using atree.
    A future change will split build-tools as well.
    
    Bug: 187222815
    Test: check that the linux/mac/windows sdk-repo-...-platform-tools-...zip
      files contain the same files
    Change-Id: I7773b321fddfc478472837d5f91f02b3b16f0bb5

---
+++ build/platform-tools.mk
@@ -0,0 +1,57 @@
+##############################################################################
+# Platform Tools Component
+#
+# Inputs:
+#   my_prefix: HOST_ or HOST_CROSS_
+##############################################################################
+
+LOCAL_NAME := platform-tools
+LOCAL_PREFIX_DIR := platform-tools
+LOCAL_FILES :=
+LOCAL_STRIP_FILES :=
+LOCAL_DIRS :=
+
+LOCAL_FILES += \
+  $(HOST_OUT_INTERMEDIATES)/NOTICE.txt:NOTICE.txt \
+  $(HOST_OUT)/development/sdk/plat_tools_source.properties:source.properties \
+  $(HOST_OUT_EXECUTABLES)/mke2fs.conf
+
+exe :=
+ifeq ($($(my_prefix)OS),windows)
+  exe := .exe
+endif
+
+LOCAL_STRIP_FILES += \
+  $($(my_prefix)OUT_EXECUTABLES)/adb$(exe) \
+  $($(my_prefix)OUT_EXECUTABLES)/fastboot$(exe) \
+  $($(my_prefix)OUT_EXECUTABLES)/mke2fs$(exe) \
+  $($(my_prefix)OUT_EXECUTABLES)/make_f2fs$(exe) \
+  $($(my_prefix)OUT_EXECUTABLES)/make_f2fs_casefold$(exe) \
+  $($(my_prefix)OUT_EXECUTABLES)/sqlite3$(exe) \
+  $($(my_prefix)OUT_EXECUTABLES)/dmtracedump$(exe) \
+  $($(my_prefix)OUT_EXECUTABLES)/etc1tool$(exe) \
+  $($(my_prefix)OUT_EXECUTABLES)/hprof-conv$(exe)
+
+ifneq ($($(my_prefix)OS),windows)
+LOCAL_STRIP_FILES += \
+  $($(my_prefix)OUT_EXECUTABLES)/e2fsdroid \
+  $($(my_prefix)OUT_EXECUTABLES)/sload_f2fs \
+  $($(my_prefix)OUT_SHARED_LIBRARIES)/libc++$($(my_prefix)SHLIB_SUFFIX):lib64/libc++$($(my_prefix)SHLIB_SUFFIX)
+else # Windows
+LOCAL_FILES += \
+  $($(my_prefix)OUT_SHARED_LIBRARIES)/AdbWinUsbApi.dll \
+  $($(my_prefix)OUT_SHARED_LIBRARIES)/AdbWinApi.dll \
+  prebuilts/gcc/linux-x86/host/x86_64-w64-mingw32-4.8/x86_64-w64-mingw32/lib32/libwinpthread-1.dll
+endif
+
+# systrace
+LOCAL_DIRS += \
+  external/chromium-trace/catapult:systrace/catapult
+LOCAL_FILES += \
+  external/chromium-trace/systrace.py:systrace/systrace.py \
+  external/chromium-trace/NOTICE:systrace/NOTICE \
+  external/chromium-trace/UPSTREAM_REVISION:systrace/UPSTREAM_REVISION
+
+exe :=
+
+include $(LOCAL_PATH)/sdk_repo.mk
