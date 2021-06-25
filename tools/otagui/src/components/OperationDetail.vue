e088059538a558bb6b9efffce5f6c4d2258b645f
lishutong <lishutong@google.com>
2021-06-25 15:50:57 +0000

    Add support for analysis of OTA package.
    payload.js defines a Class that could parse the OTA metadata from
    an OTA package(.zip). PackageAnalysis.vue enable users to upload
    their own OTA package for analysis. PayloadDetail.vue and
    OperationDetail will display the metadata in a human-readable way.
    
    The whole analysis process is done on frontend and no communication
    with server is involved after loading the page.
    
    Test: Mannual tested.
    Change-Id: I3becba872f6564ec9e44edad3a6cc19eb0714ed6

---
+++ tools/otagui/src/components/OperationDetail.vue
@@ -0,0 +1,67 @@
+<template>
+  {{ mapType.get(operation.type) }}
+  <p v-if="operation.dataOffset != null">
+    Data offset: {{ operation.dataOffset }}
+  </p>
+  <p v-if="operation.dataLength != null">
+    Data length: {{ operation.dataLength }}
+  </p>
+  <p v-if="operation.srcExtentsList != null">
+    Source: {{ operation.srcExtentsList.length }} extents ({{ srcTotalBlocks }}
+    blocks)
+    <br>
+    {{ srcBlocks }}
+  </p>
+  <p v-if="operation.dstExtentsList != null">
+    Destination: {{ operation.dstExtentsList.length }} extents ({{
+      dstTotalBlocks
+    }}
+    blocks)
+    <br>
+    {{ dstBlocks }}
+  </p>
+</template>
+
+<script>
+export default {
+  props: {
+    operation: {
+      type: Object,
+      required: true,
+    },
+    mapType: {
+      type: Map,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      srcTotalBlocks: null,
+      srcBlocks: null,
+      dstTotalBlocks: null,
+      dstBlocks: null,
+    }
+  },
+  async mounted() {
+    if (this.operation.srcExtentsList) {
+      this.srcTotalBlocks = await this.numBlocks(this.operation.srcExtentsList)
+      this.srcBlocks = await this.displayBlocks(this.operation.srcExtentsList)
+    }
+    if (this.operation.dstExtentsList) {
+      this.dstTotalBlocks = await this.numBlocks(this.operation.dstExtentsList)
+      this.dstBlocks = await this.displayBlocks(this.operation.dstExtentsList)
+    }
+  },
+  methods: {
+    async numBlocks(exts) {
+      const accumulator = async (total, ext) => (await total) + ext['numBlocks']
+      return await exts.reduce(accumulator, 0)
+    },
+    async displayBlocks(exts) {
+      const accumulator = async (total, ext) =>
+        (await total) + '(' + ext['startBlock'] + ',' + ext['numBlocks'] + ')'
+      return await exts.reduce(accumulator, '')
+    },
+  },
+}
+</script>
---
+++ tools/otagui/src/views/PackageAnalysis.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <BaseFile
+      label="Select an OTA package"
+      @file-select="unpackOTA"
+    />
+    <PayloadDetail
+      v-if="zipFile && payload"
+      :zipFile="zipFile"
+      :payload="payload"
+    />
+  </div>
+</template>
+
+<script>
+import BaseFile from '@/components/BaseFile.vue'
+import PayloadDetail from '@/components/PayloadDetail.vue'
+import { Payload } from '@/services/payload.js'
+
+export default {
+  components: {
+    BaseFile,
+    PayloadDetail,
+  },
+  data() {
+    return {
+      zipFile: null,
+      payload: null,
+    }
+  },
+  methods: {
+    async unpackOTA(files) {
+      this.zipFile = files[0]
+      try {
+        this.payload = await new Payload(this.zipFile)
+        this.payload.init()
+      } catch (err) {
+        console.log(err)
+      }
+    },
+  },
+}
+</script>
