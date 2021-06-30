f19f0305648b56924e866f98cd6c57e8603e62e4
Weilun Du <wdu@google.com>
2021-06-30 08:32:31 -0700

    [SdkSetup]  Clean up
    1, Fixed AndroidManifests.xml parsing error.
    2, Connect to AndroidWifi access point for emulator only.
    
    Bug: 185242318
    
    Signed-off-by: Weilun Du <wdu@google.com>
    Change-Id: I5d4863b3c5aba8e7dfccdd061739afe30ded5162

--- apps/SdkSetup/src/com/android/sdksetup/DefaultActivity.java
+++ apps/SdkSetup/src/com/android/sdksetup/DefaultActivity.java
@@ -78,17 +78,18 @@ public class DefaultActivity extends Activity {
 
             TelephonyManager mTelephony = getApplicationContext().getSystemService(TelephonyManager.class);
             mTelephony.setPreferredNetworkTypeBitmask(TelephonyManager.NETWORK_TYPE_BITMASK_NR);
+
+            // Add network with SSID "AndroidWifi"
+            WifiConfiguration config = new WifiConfiguration();
+            config.SSID = "\"AndroidWifi\"";
+            config.setSecurityParams(WifiConfiguration.SECURITY_TYPE_OPEN);
+            WifiManager mWifiManager = getApplicationContext().getSystemService(WifiManager.class);
+            int netId = mWifiManager.addNetwork(config);
+            if (netId == ADD_NETWORK_FAIL || mWifiManager.enableNetwork(netId, true)) {
+                Log.e(TAG, "Unable to add Wi-Fi network AndroidWifi.");
+            }
         }
 
-        // Add network with SSID "AndroidWifi"
-        WifiConfiguration config = new WifiConfiguration();
-        config.SSID = "\"AndroidWifi\"";
-        config.setSecurityParams(WifiConfiguration.SECURITY_TYPE_OPEN);
-        WifiManager mWifiManager = getApplicationContext().getSystemService(WifiManager.class);
-        int netId = mWifiManager.addNetwork(config);
-        if (netId == ADD_NETWORK_FAIL || mWifiManager.enableNetwork(netId, true)) {
-            Log.e(TAG, "Unable to add Wi-Fi network AndroidWifi.");
-        }
         // remove this activity from the package manager.
         PackageManager pm = getPackageManager();
         ComponentName name = new ComponentName(this, DefaultActivity.class);
